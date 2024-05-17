ac2e4ba8e3332e47edc13644cdac2d935c5334e6
mushu1025 <yangjun.314@163.com>
2024-05-17 17:49:11 +0800

    update

---
+++ PatchGenerateJobs.groovy
@@ -0,0 +1,32 @@
+String folderNameStr = "$FolderName"
+String executeCommands = "$ExecuteCommands"
+String remoteIPsStr = "$RemoteIPs"
+String delimiter = ','
+List<String> remoteIPs = remoteIPsStr.split(delimiter)
+println remoteIPs
+
+folder(folderNameStr) {
+    displayName(folderNameStr)
+    description(folderNameStr)
+}
+
+job(folderNameStr+'/TriggerAll') {
+}
+
+for (int i = 0; i < remoteIPs.size(); i++) {
+    String jobName = remoteIPs.get(i)
+    job(folderNameStr+'/'+jobName) {
+        triggers {
+            upstream('TriggerAll', 'SUCCESS')
+        }
+        steps {
+            sshBuilder {
+                siteName('root@192.168.3.119:22')
+                command(executeCommands)
+                // Execute each line in the script individually.
+                execEachLine(false)
+                hideCommand(false)
+            }
+        }
+    }
+}

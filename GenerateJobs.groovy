80ab2f8184380a414bc01d2947d21490dc91ef16
mushu1025 <yangjun.314@163.com>
2024-05-17 12:33:13 +0800

    update

--- GenerateJobs.groovy
+++ GenerateJobs.groovy
@@ -12,20 +12,32 @@ pipelineJob('project-a/192.168.0.1') {
     }
 }
 
-pipelineJob('project-a/192.168.0.2') {
+job('project-b/192.168.0.1') {
     triggers {
         upstream('TriggerAll', 'SUCCESS')
     }
-}
-
-job('project-a/192.168.0.3') {
-    triggers {
-        upstream('TriggerAll', 'SUCCESS')
+    steps {
+        sshBuilder {
+            siteName('root@192.168.3.119:22')
+            command('ls  -trl')
+            // Execute each line in the script individually.
+            execEachLine(false)
+            hideCommand(false)
+        }
     }
 }
 
-job('project-a/192.168.0.4') {
+job('project-b/192.168.0.2') {
     triggers {
         upstream('TriggerAll', 'SUCCESS')
+    }
+    steps {
+        sshBuilder {
+            siteName('root@192.168.3.119:22')
+            command('ls  -trl')
+            // Execute each line in the script individually.
+            execEachLine(false)
+            hideCommand(false)
+        }
     }
 }
